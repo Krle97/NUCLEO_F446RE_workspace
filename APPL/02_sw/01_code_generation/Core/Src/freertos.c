316c085aeeaa57140666c85be46259ba7740b77a
Krle97 <igorkrstic.sm@gmail.com>
2023-01-11 20:43:45 +0100

    [Software] Adding a missing include

--- APPL/02_sw/01_code_generation/Core/Src/freertos.c
+++ APPL/02_sw/01_code_generation/Core/Src/freertos.c
@@ -25,7 +25,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "LEDM.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
